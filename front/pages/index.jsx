0a5dd6bf904e92a3867c462c0145690059be05c3
aacc1110 <tadrow@daum.net>
2019-05-04 13:45:38 +0900

    sns

---
+++ front/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppLayout from '../components/AppLaout';
+
+const Home = () => {
+  return (
+    <>
+      <AppLayout>
+        <div> Hello, Next</div>
+      </AppLayout>
+    </>
+  );
+};
+
+export default Home;
