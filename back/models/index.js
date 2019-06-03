7e89d7c20651635acffc4ff3c9778d9c3f8267b6
aacc1110 <tadrow@daum.net>
2019-06-03 09:34:49 +0900

    last

--- back/models/index.js
+++ back/models/index.js
@@ -1,8 +1,6 @@
-const path = require('path');
 const Sequelize = require('sequelize');
-
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+const config = require('../config/config')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
@@ -14,9 +12,9 @@ db.Post = require('./post')(sequelize, Sequelize);
 db.User = require('./user')(sequelize, Sequelize);
 
 Object.keys(db).forEach(modelName => {
-	if (db[modelName].associate) {
-		db[modelName].associate(db);
-	}
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
